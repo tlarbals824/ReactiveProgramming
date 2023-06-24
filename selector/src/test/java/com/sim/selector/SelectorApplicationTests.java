b25557374fbc169be1531669bc04b0b45689590f
tlarbals824 <simkm0824@naver.com>
2023-06-24 11:06:53 +0900

    [feat] selector을 통한 non-blocking io 최적화

---
+++ selector/src/test/java/com/sim/selector/SelectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sim.selector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SelectorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
