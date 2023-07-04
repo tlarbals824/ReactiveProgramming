8bab5b8d12ea116df06b0cc78b28fb08afcae304
tlarbals824 <simkm0824@naver.com>
2023-07-04 17:43:19 +0900

    [feat] 멀티 모듈 기반 Webflux 서버 구성

---
+++ webfluxserver/Webflux-Image/src/main/java/com/sim/webfluximage/WebfluxImageApplication.java
@@ -0,0 +1,13 @@
+package com.sim.webfluximage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxImageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxImageApplication.class, args);
+	}
+
+}
---
+++ webfluxserver/Webflux-Image/src/test/java/com/sim/webfluximage/WebfluxImageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sim.webfluximage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxImageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
